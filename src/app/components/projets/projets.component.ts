89eb504aae7fd6929600900767fce0870b680fd8
RomainChanteloup <chanteloup.romain@gmail.com>
2023-02-01 15:56:23 +0100

    Now ETP by iteration working

--- src/app/components/projets/projets.component.ts
+++ src/app/components/projets/projets.component.ts
@@ -13,14 +13,14 @@ export class ProjetsComponent implements OnInit {
   ngOnInit(): void {
     this.directusService.getPlanStaff().subscribe((data: any) => {
       this.planStaff = data.data;
-      this.loadChartData();
-    });
-
-    this.directusService.getIterations().subscribe((data: any) => {
-      console.log(data.data);
+      this.directusService.getIterations().subscribe((data: any) => {
+        this.iterationsList = data.data;
+        this.loadChartData();
+      });
     });
   }
   planStaff: any;
+  iterationsList: any;
   chartLoaded = false;
 
   barChartOptions: ChartConfiguration['options'] = {
@@ -67,28 +67,36 @@ export class ProjetsComponent implements OnInit {
     //   ETP: number,
     // }
     //make some opperation to get all nescessary data
+    let distinctPIGlobal: any[] = [
+      ...new Set(this.iterationsList.map((item: { Label: any }) => item.Label)),
+    ];
+
     let sumETP: any = [];
     for (let record of this.planStaff) {
-      let projectAndPiAlreadyThere = sumETP.find(
-        (el: any) =>
-          el.PI === record.Iteration_debut.Label &&
-          el.Projet === record.Projet.Nom
+      let iterationOfRecord = this.getIterationsFromStartEnd(
+        record.Iteration_debut.Label,
+        record.Iteration_fin.Label,
+        distinctPIGlobal
       );
-      if (!projectAndPiAlreadyThere) {
-        sumETP.push({
-          PI: record.Iteration_debut.Label,
-          Projet: record.Projet.Nom,
-          ETP: parseInt(record.Charge),
-        });
-      } else {
-        projectAndPiAlreadyThere.ETP += parseInt(record.Charge);
+      for (let iterationLabel of iterationOfRecord) {
+        let projectAndPiAlreadyThere = sumETP.find(
+          (el: any) =>
+            el.PI === iterationLabel && el.Projet === record.Projet.Nom
+        );
+
+        if (!projectAndPiAlreadyThere) {
+          sumETP.push({
+            PI: iterationLabel,
+            Projet: record.Projet.Nom,
+            ETP: parseInt(record.Charge),
+          });
+        } else {
+          projectAndPiAlreadyThere.ETP += parseInt(record.Charge);
+        }
       }
     }
 
     // now loading data in te chart
-    let distinctPI: any[] = [
-      ...new Set(sumETP.map((item: { PI: any }) => item.PI)),
-    ];
     let distinctProjects = [
       ...new Set(sumETP.map((item: { Projet: any }) => item.Projet)),
     ];
@@ -97,18 +105,66 @@ export class ProjetsComponent implements OnInit {
     for (let projet of distinctProjects) {
       let projetNom = projet;
       let listeETP: any[] = [];
-      for (let it of distinctPI) {
-        if (sumETP.find((el: any) => el.PI === it && el.Projet === projet)) {
-          listeETP.push(it);
+      for (let it of distinctPIGlobal) {
+        let corespondingETP = sumETP.find(
+          (el: any) => el.PI === it && el.Projet === projet
+        );
+        if (corespondingETP) {
+          listeETP.push(corespondingETP.ETP);
         } else {
           listeETP.push(0);
         }
       }
       datasetList.push({ data: listeETP, label: projetNom });
     }
-    this.barChartData = { labels: distinctPI, datasets: datasetList };
+    this.barChartData = { labels: distinctPIGlobal, datasets: datasetList };
     this.chartLoaded = true;
   }
 
-  getIterationsFromStartEnd(start: string, end: string) {}
+  getIterationsFromStartEnd(
+    start: string,
+    end: string,
+    iterationList: string[]
+  ) {
+    let firstIteration = iterationList.find((el) => el === start);
+    let lastIteration = iterationList.find((el) => el === end);
+    if (!firstIteration || !lastIteration) {
+      // not start increment or end increment valid
+      return [];
+    }
+    let res: string[] = [firstIteration];
+    let currentIteration = firstIteration;
+    while (currentIteration !== lastIteration) {
+      let actualIncrement = currentIteration.substring(
+        0,
+        currentIteration.indexOf('.')
+      );
+      let actualIteration = currentIteration.substring(
+        currentIteration.indexOf('.') + 1
+      );
+      // case 1.1 and next is 1.2
+      let nextIterPossible =
+        actualIncrement + '.' + (parseInt(actualIteration) + 1).toString();
+      let nextIter = iterationList.find((el) => el === nextIterPossible);
+      if (nextIter) {
+        res.push(nextIter);
+        currentIteration = nextIter;
+        continue;
+      } else {
+        // case 1.1 and next is 2.1
+        let nextIterPossible =
+          (parseInt(actualIncrement) + 1).toString() + '.1';
+        let nextIter = iterationList.find((el) => el === nextIterPossible);
+        if (nextIter) {
+          res.push(nextIter);
+          currentIteration = nextIter;
+          continue;
+        } else {
+          // case there is no 1.2 or 2.1 so the current iter is the last
+          return res;
+        }
+      }
+    } //case where start is the same as the end
+    return res;
+  }
 }
